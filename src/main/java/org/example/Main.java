c9fb10bd800939bc7a419d967a92de882f675a47
Dmitriy Tarasov <dmitriy.tarasov@itv.ru>
2025-05-06 17:09:56 +0700

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,51 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build()) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                // Проверка успешности запроса
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    System.err.println("Ошибка при получении данных: " + response.getStatusLine());
+                    return;
+                }
+
+                // Парсинг JSON
+                HttpEntity entity = response.getEntity();
+                ObjectMapper mapper = new ObjectMapper();
+                InputStream content = entity.getContent();
+
+                List<CatFact> facts = mapper.readValue(content, new TypeReference<List<CatFact>>() {});
+
+                // Фильтрация и вывод
+                facts.stream()
+                        .filter(fact -> fact.getUpvotes() != null)
+                        .forEach(System.out::println);
+            }
+        } catch (Exception e) {
+            System.err.println("Произошла ошибка: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
